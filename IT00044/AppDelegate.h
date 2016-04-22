26dbe9066734a6aa608dbc8c3171560620de6c91
cvenji <1345250384@qq.com>
2016-04-22 15:25:47 +0800

    ff
    33

---
+++ IT00044/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT00044
+//
+//  Created by Mac on 16/4/22.
+//  Copyright © 2016年 JiaJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IT00044/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT00044
+//
+//  Created by Mac on 16/4/22.
+//  Copyright © 2016年 JiaJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
